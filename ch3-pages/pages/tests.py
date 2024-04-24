17329f31f298030c9983f237f4b8e515e73af5af
MOHAMedelhennawy <el7ennaw2002@gmail.com>
2024-04-24 14:29:25 +0200

    Add Testing

--- ch3-pages/pages/tests.py
+++ ch3-pages/pages/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.test import SimpleTestCase
 
-# Create your tests here.
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        respons = self.client.get('/')
+        self.assertEqual(respons.status_code, 200)
+    
+    def test_about_page_status_code(self):
+        respons = self.client.get('/about/')
+        self.assertEqual(respons.status_code, 200)
